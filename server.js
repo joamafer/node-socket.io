094ce7f798138f0ffaa4090b77978f911f112c17
Jose Antonio <jose@clearscore.com>
2017-04-02 12:03:09 +0100

    t

--- server.js
+++ server.js
@@ -3,19 +3,17 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
+const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-var bodyParser = require('body-parser')
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 server.use(bodyParser.json());       // to support JSON-encoded bodies
-// server.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-//   extended: true
-// }));
+server.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 // server.post('/test-page', function(req, res) {
 //     console.log("recibido algo!!!!!!")
