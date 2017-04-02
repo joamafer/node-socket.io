583848aa0712fc2e01a4de1c0b5b41bf58ab509f
Jose Antonio <jose@clearscore.com>
2017-04-02 11:48:58 +0100

    Y

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const socketIO = require('socket.io');
-const fs = require('fs');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
@@ -12,16 +11,16 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-server.get('/', function(req, res){
-    console.log('JOSE GET /')
-    var html = fs.readFileSync('index.html');
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(html);
-});
+// server.get('/', function(req, res){
+//     console.log('JOSE GET /')
+//     var html = fs.readFileSync('index.html');
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.end(html);
+// });
 
-server.post('/', function(req, res){
-    console.log('JOSE POST /');
-    console.dir(req.body);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('thanks');
-});
+// server.post('/', function(req, res){
+//     console.log('JOSE POST /');
+//     console.dir(req.body);
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.end('thanks');
+// });
