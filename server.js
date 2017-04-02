7d85aca6aad1352fa91ed00f9d6f07bdc7d4101f
Jose Antonio <jose@clearscore.com>
2017-04-02 10:54:01 +0100

    Test

--- server.js
+++ server.js
@@ -19,3 +19,24 @@ io.on('connection', (socket) => {
 });
 
 setInterval(() => io.emit('guarra', new Date().toTimeString()), 1000);
+
+var http = require('http');
+    var server = http.createServer ( function(request,response){
+
+    response.writeHead(200,{"Content-Type":"text\plain"});
+    if(request.method == "GET")
+        {
+            response.end("received GET request.")
+        }
+    else if(request.method == "POST")
+        {
+            response.end("received POST request.");
+        }
+    else
+        {
+            response.end("Undefined request .");
+        }
+});
+
+server.listen(8000);
+console.log("Server running on port 8000");
