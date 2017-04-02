32f1ab3d3fe930c7c6654ae8fa49f576d9517fc2
Jose Antonio <jose@clearscore.com>
2017-04-02 12:43:18 +0100

    Test

--- server.js
+++ server.js
@@ -3,27 +3,29 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const http = require('http');
+
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
-const server = express()
-server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-server.use(bodyParser.json());       // to support JSON-encoded bodies
-server.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+const app = express();
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
 // Socket.IO
-const io = socketIO.listen(server);
+const io = socketIO.listen(http.createServer(app));
 
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-server.post('/stories', function(req, res) {
-    console.log(req.body.result.action)
-    io.emit('action', req.body.result.action)
+app.post('/stories', function(req, res) {
+    console.log(req.body.result.action);
+    io.emit('action', req.body.result.action);
 });
